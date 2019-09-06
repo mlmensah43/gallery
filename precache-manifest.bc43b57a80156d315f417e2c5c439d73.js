b84a57d78b3dddc4a97436852a0e7b865695df8d
Mariah Mensah <mariahm207@gmail.com>
2019-09-06 12:09:00 -0500

    Updates

---
+++ precache-manifest.bc43b57a80156d315f417e2c5c439d73.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a4cf02f18b3c7c63d134469f1c2196",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "c9b59dc7a24659a9dc71",
+    "url": "/gallery/static/css/main.c9a408ac.chunk.css"
+  },
+  {
+    "revision": "23499fec9fd0ba4357dc",
+    "url": "/gallery/static/js/2.aec75699.chunk.js"
+  },
+  {
+    "revision": "c9b59dc7a24659a9dc71",
+    "url": "/gallery/static/js/main.ae454a4b.chunk.js"
+  },
+  {
+    "revision": "de9a95ae35c4a918bf4e",
+    "url": "/gallery/static/js/runtime~main.3e4d5bb6.js"
+  },
+  {
+    "revision": "193af9aba0ae7c493599140ee36b3658",
+    "url": "/gallery/static/media/blue.193af9ab.jpg"
+  },
+  {
+    "revision": "35198c88cafee3ba7b6e59ac624f6fbf",
+    "url": "/gallery/static/media/fish.35198c88.jpg"
+  },
+  {
+    "revision": "df4c439f5170adc4f9ce14023cfb85b3",
+    "url": "/gallery/static/media/icey.df4c439f.jpg"
+  },
+  {
+    "revision": "cc25eee46a5f5311402cf9529cf1303c",
+    "url": "/gallery/static/media/jellyfish.cc25eee4.jpg"
+  },
+  {
+    "revision": "0c708ab6905ebdc2036593725b2aab63",
+    "url": "/gallery/static/media/mountains.0c708ab6.jpg"
+  },
+  {
+    "revision": "114628dbea89b3f515c0a10a8fbe1f64",
+    "url": "/gallery/static/media/painting.114628db.jpg"
+  },
+  {
+    "revision": "e291f60363d0ce81cf1445b379fb7726",
+    "url": "/gallery/static/media/smokey.e291f603.jpg"
+  },
+  {
+    "revision": "9c19dba47292c1db2de98b52f1de8551",
+    "url": "/gallery/static/media/snowflake.9c19dba4.jpg"
+  },
+  {
+    "revision": "0bb4d5f4bbc4e887fa5b33f7551e2e97",
+    "url": "/gallery/static/media/square.0bb4d5f4.jpg"
+  },
+  {
+    "revision": "78ce86d100c39ce563dd0e02345e4262",
+    "url": "/gallery/static/media/starfish.78ce86d1.jpg"
+  },
+  {
+    "revision": "5ddba7ead89fb5a5992e28f78d0199c8",
+    "url": "/gallery/static/media/top.5ddba7ea.jpg"
+  },
+  {
+    "revision": "9f120aa1419945ed30b09d77fa8d5261",
+    "url": "/gallery/static/media/view.9f120aa1.jpg"
+  }
+]);
